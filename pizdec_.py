b3265e1cd189a93caf0386a63dc5beefd00d0fa5
Loligunter <137041703+Loligunter@users.noreply.github.com>
2024-12-11 00:36:42 +0300

    Add files via upload

--- pizdec_.py
+++ pizdec_.py
@@ -1,26 +1,23 @@
 import requests
-from LxmlSoup import LxmlSoup
+from bs4 import BeautifulSoup  
 import json
 
 url = "https://www.banki.ru/products/currency/cb/"
 
 
 html = requests.get(url).text
-soup = LxmlSoup(html)
-dct={}
 
-links = soup.find_all('div', class_= 'GridCol__sc-n5ivvz-0 jnxKZK')
+soup = BeautifulSoup(html, 'lxml')
+dct = {}
 
+# Ищем все блоки с курсами валют
+names = [i.get_text(strip=True) for i in soup.find_all('div', class_='Text__sc-vycpdy-0 bmceOm') if len(i.get_text(strip=True)) == 3]  # Названия валют
+buy_rates = [i.get_text(strip=True) for i in soup.find_all('div', class_='Text__sc-vycpdy-0 gJTmbP')]  # Курс покупки
 
+# Заполняем словарь курсами валют
+for name, buy_rate in zip(names, buy_rates):
+    dct[name] = buy_rate
 
-for link in links:
-    name = link.text() 
-    a = soup.find_all('div', class_='Text__sc-vycpdy-0 bmceOm')#название валюты
-    b = soup.find_all('div', class_="Text__sc-vycpdy-0 gJTmbP")#курс покупки
-    for item_a, item_b in zip(a, b):
-        dct[item_a.text()]= item_b.text()
-
-
-json_data = json.dumps(dct, ensure_ascii=False, indent=4)
+# Записываем данные в JSON файл
 with open("data.json", "w", encoding="utf-8") as json_file:
-    json_file.write(json_data)
+    json.dump(dct, json_file, ensure_ascii=False, indent=4)
